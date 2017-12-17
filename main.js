b7c116bd2ccd0aeb6ab606acb3c0e78a7999003f
rathereasy@gmail.com <jacques@decodemtl>
2017-12-17 18:05:49 -0500

    Question 5

--- main.js
+++ main.js
@@ -4,20 +4,30 @@ var mainDiv = document.getElementById('app');
 var body = document.getElementById('main');
 var btn = document.getElementById('btn');
 var start = document.getElementById('start');
+var restart = document.getElementById('restart')
 
 btn.style.display = "none"
 btn.style.left = (100 + Math.random() * 400) + 'px';
 btn.style.top = (100 + Math.random() * 400) + 'px';
+
+restart.style.display="none";
+
+function restartAppear() {
+    restart.style.display = "";
+}
+
 function potentialLoss() {
     if (won) return;
     lost = true;
     mainDiv.innerText = "YOU LOST!";
+    restartAppear();
 }
 
 function potentialWin() {
     if (lost) return;
     won = true;
     mainDiv.innerText = "YOU WON!";
+    restartAppear();
 }
 
 function startRound() {
@@ -36,3 +46,7 @@ start.addEventListener('click', () => {
     start.style.display = "none";
     setTimeout(startRound, Math.random() * 2000 + 1000)
 });
+
+restart.addEventListener('click', () => {
+    location.reload();
+})
