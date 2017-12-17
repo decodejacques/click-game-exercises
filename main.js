5636b9cea484dccdd13bb80d3f21c0fe5f850c2d
rathereasy@gmail.com <jacques@decodemtl>
2017-12-17 16:01:05 -0500

    question 2

--- main.js
+++ main.js
@@ -1,12 +1,33 @@
+var won = false;
 var lost = false;
+var started = false;
 var mainDiv = document.getElementById('app');
 var body = document.getElementById('main');
-body.addEventListener('click', () => {
-    mainDiv.innerText = "YOU WON!";
-    won = true
-})
-function checkWon() {
-    if (lost) { mainDiv.innerText = "YOU LOST!"; }
 
+function potentialLoss() {
+    if (won) return;
+    lost = true;
+    mainDiv.innerText = "YOU LOST!";
 }
-setTimeout(checkWon, 1000);
+
+function potentialWin() {
+    if (lost) return;
+    won = true;
+    mainDiv.innerText = "YOU WON!";
+}
+
+function startRound() {
+    mainDiv.innerText = "ROUND STARTED!";
+
+    body.addEventListener('click', () => {
+        potentialWin();
+    })
+    body.addEventListener('keydown', event => {
+        if (event.key == " ") {
+            potentialWin();
+        }
+    })
+    setTimeout(potentialLoss, 500)
+}
+
+setTimeout(startRound, Math.random() * 2000 + 1000);
